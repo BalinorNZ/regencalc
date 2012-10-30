a475e53f3b6f4edad989cf7a5ec343719abd7b9d
Nic Bathate <balinornz@gmail.com>
2012-10-31 04:12:31 +1300

    added some variables, made list of modules to install

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', base: '16', gain: '1.8', hi: 'bye' });
+};
+
+exports.about = function(req, res){
+  res.render('index', { title: 'About', base: '21', gain: '3.2', hi: 'bye'  })
 };
