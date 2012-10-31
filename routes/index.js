517ef8dd7f9391738fe38cbabfd59959f93fddab
Nic Bathgate <balinornz@gmail.com>
2012-10-31 18:10:55 +1300

    put stuff in a table, made a foreach loop

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', base: '16', gain: '1.8', hi: 'bye' });
+  res.render('index', { title: 'Regen Calc', base: 16, gain: 1.8, cost: 140,
+		colors: [ { name: 'Red' }, { name: 'Blue' }, { name: 'Green' } ] })
 };
 
 exports.about = function(req, res){
-  res.render('index', { title: 'About', base: '21', gain: '3.2', hi: 'bye'  })
+  res.render('index', { title: 'About', base: 21, gain: 3.2, hi: 'bye'  })
 };
