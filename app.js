f16713c120524062982e92b1fb63a044182210f1
Nic Bathgate <balinornz@gmail.com>
2012-11-01 17:37:28 +1300

    trying to switch to dust instead of ejs

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , cons = require('consolidate');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'dust');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@
  */
 
 exports.index = function(req, res){
+	console.log(req.url);
   res.render('index', { title: 'Regen Calc', base: 16, gain: 1.8, cost: 140,
 		colors: [ { name: 'Red' }, { name: 'Blue' }, { name: 'Green' } ] })
 };
 
 exports.about = function(req, res){
+	console.log(req.url);
   res.render('index', { title: 'About', base: 21, gain: 3.2, hi: 'bye'  })
 };
