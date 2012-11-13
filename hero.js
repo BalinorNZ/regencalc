e804c1a057c03758fee87fe5150480e4c9d94a34
Nic Bathgate <balinornz@gmail.com>
2012-11-14 12:19:23 +1300

    trying out toFixed() for rounding

--- hero.js
+++ hero.js
@@ -13,9 +13,9 @@ exports.Hero.prototype.getLevels = function() {
 	var levels = new Array();
 	for(var i=0; i<25; i++) {
 		var intel = Math.round((this.base+i*this.gain)*10)/10;
-		var regen = Math.round(((intel*0.04)+0.01)*100)/100;
+		var regen = (intel*0.04)+0.01;//Math.round(((intel*0.04)+0.01)*100)/100;
 		var time = Math.round((this.cost/regen)*10)/10;
-		levels[i] = { 'level': i+1, 'intel': intel, 'regen': regen, 'time': time };
+		levels[i] = { 'level': i+1, 'intel': intel, 'regen': regen.toFixed(2), 'time': time };
 	}
 	return levels;
 }
