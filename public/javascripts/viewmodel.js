3bc8f5c78f1dac67e0737ce289b204190748c690
Nic Bathgate <balinornz@gmail.com>
2012-11-15 12:00:55 +1300

    knockout fully implemented

--- public/javascripts/viewmodel.js
+++ public/javascripts/viewmodel.js
@@ -1,69 +1,46 @@
 $(document).ready(function(){ 
-    function Task(data) {
-        this.title = ko.observable(data.title);
-        this.isDone = ko.observable(data.isDone);
-    }
-
     function Level(data) {
-        this.level = ko.computed(function() {
-            return data.level+1;
-        });
+        this.level = ko.observable(data.level);
         this.intel = ko.computed(function() {
-            return this.base+data.level*this.gain;
-        });
+            var intel = data.base() + (data.level - 1) * data.gain();
+            return intel.toFixed(1);
+        }, this);
         this.regen = ko.computed(function() {
-            return parseFloat(data.gain);
-        });
+            var regen = (this.intel() * 0.04) + 0.01;
+            return regen.toFixed(2);
+        }, this);
         this.time = ko.computed(function() {
-            return parseFloat(data.level)+1;
-        });
+            var time = data.cost() / this.regen();
+            return time.toFixed(0);
+        }, this);
     }
 
-    function TaskListViewModel() {
+    function LevelsViewModel() {
         this.base = ko.numericObservable(16);
         this.gain = ko.numericObservable(1.8);
         this.cost = ko.numericObservable(140);
+
         this.levels = ko.observableArray([]);
         for(var i=0; i<25; i++) {
-            var intel = Math.round((this.base+i*this.gain)*10)/10;
-            var regen = (intel*0.04)+0.01;//Math.round(((intel*0.04)+0.01)*100)/100;
-            var time = Math.round((this.cost/regen)*10)/10;
-            this.levels.push(new Level({ level: i, base: this.base, gain: this.gain, cost: this.cost }));
+            this.levels.push(new Level({ level: i+1, base: this.base, gain: this.gain, cost: this.cost }));
         }
-
-        var self = this;
-        self.tasks = ko.observableArray([]);
-        self.newTaskText = ko.observable();
-        self.incompleteTasks = ko.computed(function() {
-            return ko.utils.arrayFilter(self.tasks(), function(task) { return !task.isDone() });
-        });
-
-        // Operations
-        self.addTask = function() {
-            self.tasks.push(new Task({ title: this.newTaskText() }));
-            self.newTaskText("");
-        };
-        self.removeTask = function(task) { self.tasks.remove(task) };
     }
 
     ko.numericObservable = function(initialValue) {
-    // "shim" observable constructor to automatically store numeric strings as numbers
-    // html elements always store values as strings, which is annoying for numbers
-        
-    var _actual = ko.observable(initialValue);
+        // "shim" observable constructor to automatically store numeric strings as numbers
+        // html elements always store values as strings, which is annoying for numbers
+        var _actual = ko.observable(initialValue);
+        var result = ko.dependentObservable({
+            read: function() {
+                return _actual();
+            },
+            write: function(newValue) {
+                var parsedValue = parseFloat(newValue);
+                _actual(isNaN(parsedValue) ? newValue : parsedValue);
+            }
+        });
+        return result;
+    };
 
-    var result = ko.dependentObservable({
-        read: function() {
-            return _actual();
-        },
-        write: function(newValue) {
-            var parsedValue = parseFloat(newValue);
-            _actual(isNaN(parsedValue) ? newValue : parsedValue);
-        }
-    });
-
-    return result;
-};
-
-    ko.applyBindings(new TaskListViewModel());
+    ko.applyBindings(new LevelsViewModel());
 });
