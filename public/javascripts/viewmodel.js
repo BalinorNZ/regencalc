4670cae8451155c5d4ef5686c994c706631dd54d
Nic Bathgate <balinornz@gmail.com>
2012-11-15 16:50:36 +1300

    WIP drag and drop

--- public/javascripts/viewmodel.js
+++ public/javascripts/viewmodel.js
@@ -1,4 +1,34 @@
-$(document).ready(function(){ 
+(function() {
+var _dragged;
+ko.bindingHandlers.drag = {
+    init: function(element, valueAccessor, allBindingsAccessor, LevelsViewModel) {
+        var dragElement = $(element);
+        var dragOptions = {
+            helper: 'clone',
+            start: function() {
+                _dragged = ko.utils.unwrapObservable(valueAccessor().value);
+            },
+            cursor: 'default'
+        };
+        dragElement.draggable(dragOptions).disableSelection();
+    }
+};
+
+ko.bindingHandlers.drop = {
+    init: function(element, valueAccessor, allBindingsAccessor, LevelsViewModel) {
+        var dropElement = $(element);
+        var dropOptions = {
+            drop: function(event, ui) {
+                valueAccessor().value(_dragged);
+            }
+        };
+        dropElement.droppable(dropOptions);
+    }
+};
+})();
+
+$(document).ready(function(){
+
     function Level(data) {
         this.level = ko.observable(data.level);
         this.intel = ko.computed(function() {
@@ -15,15 +45,51 @@ $(document).ready(function(){
         }, this);
     }
 
-    function LevelsViewModel() {
-        this.base = ko.numericObservable(16);
-        this.gain = ko.numericObservable(1.8);
-        this.cost = ko.numericObservable(140);
+    function Hero(data) {
+        this.name = data.name;
+        this.base = data.base;
+        this.gain = data.gain;
+        this.cost = data.cost;
+    }
 
-        this.levels = ko.observableArray([]);
+    function Item(data) {
+        this.name = data.name;
+    }
+
+    function LevelsViewModel() {
+        // Data
+        var self = this;
+        self.base = ko.numericObservable(10);
+        self.gain = ko.numericObservable(1);
+        self.cost = ko.numericObservable(100);
+
+        self.levels = ko.observableArray([]);
         for(var i=0; i<25; i++) {
-            this.levels.push(new Level({ level: i+1, base: this.base, gain: this.gain, cost: this.cost }));
+            self.levels.push(new Level({ level: i+1, base: self.base, gain: self.gain, cost: self.cost }));
         }
+
+        self.heroes = ko.observableArray([
+            new Hero({ name: 'Lina', base: 27, gain: 3.2, cost: 265}),
+            new Hero({ name: 'Sandking', base: 16, gain: 1.8, cost: 140}),
+        ]);
+
+        self.items = ko.observableArray([ new Item({ name: "test" }) ]);
+        self.latestItem = ko.computed({
+            read: function() {
+                return self.items().length ? self.items()[0] : "";
+            },
+            write: function(value) {
+                this.items.unshift(value);
+            },
+            owner: this
+        });
+
+        // Operations
+        self.loadHero = function(hero) {
+            self.base(hero.base);
+            self.gain(hero.gain);
+            self.cost(hero.cost);
+        };
     }
 
     ko.numericObservable = function(initialValue) {
@@ -44,3 +110,20 @@ $(document).ready(function(){
 
     ko.applyBindings(new LevelsViewModel());
 });
+
+/*
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("Text", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.originalEvent.dataTransfer.getData("Text");
+        LevelsViewModel.items.push(new Item({ name: data }));
+        ev.target.appendChild(document.getElementById(data));
+    }
+*/
